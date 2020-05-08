392271615113ff49647878cf3f90ae6d8744ede6
1000ship <cjstjdgur1234@gmail.com>
2020-05-09 01:29:10 +0900

    Add 15657.py

---
+++ 15657.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+sub = list(map(int, input().split()))
+sub.sort()
+
+def recursion( arr, start ):
+    global n, m
+    if len(arr) < m:
+        for i in range(start, n+1):
+            recursion( arr+[i], i )
+    else:
+        print( " ".join(map(lambda x: str(sub[x-1]), arr)) )
+
+recursion([], 1)
